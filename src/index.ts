8a3db8e233b0373b36cd018c29446ddc92f0b7cc
Alex Renoki <alex@renoki.org>
2023-07-11 20:45:42 +0300

    Export metrics

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * as Brain from './brain';
 export * as Gossiper from './gossiper';
+export * as Metrics from './metrics';
 export * as Pusher from './pusher';
 export * as Webhooks from './webhooks';
 export * as WS from './ws';
